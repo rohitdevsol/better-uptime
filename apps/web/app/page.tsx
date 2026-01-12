7e733132d5c785f75830a5de16658221051a7ca6
Rohit <rohitnarnolia88@gmail.com>
2026-01-12 06:54:11 +0530

    add shadcn

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,16 @@
-import { prisma } from "@repo/database";
+import { Button } from "@/components/ui/button";
+// import { prisma } from "@repo/database";
 
 export default async function IndexPage() {
-  const users = await prisma.user.findMany();
+  // const users = await prisma.user.findMany();
 
   return (
     <div>
       <h1>Hello World</h1>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
+      <Button variant={"default"}>Button</Button>
+      {/* <p className="text-red-400 bg-red-700">hello</p> */}
+
+      {/* <pre>{JSON.stringify(users, null, 2)}</pre> */}
     </div>
   );
 }
