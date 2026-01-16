4fee9417e9c6d0bade75a5ff7491ec7f74912dcd
Rohit <rohitnarnolia88@gmail.com>
2026-01-16 08:23:29 +0530

    refactor package.json

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,8 +4,6 @@ import morgan from "morgan";
 // import "express-async-errors";
 import { errorHandler } from "@/middlewares/error.middleware";
 import { v1Router } from "@/routers/v1";
-import { toNodeHandler } from "better-auth/node";
-import { auth } from "./lib/auth";
 
 const app = express();
 app.use(morgan("dev"));
